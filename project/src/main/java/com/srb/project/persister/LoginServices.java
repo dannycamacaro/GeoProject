db9c81d7a249a946c808a02d1f5b71c4438d6492
dacamacaro <dacamacaro@consisint.com>
2018-10-11 20:41:28 -0500

    ajuste de vista login

---
+++ project/src/main/java/com/srb/project/persister/LoginServices.java
@@ -0,0 +1,56 @@
+package com.srb.project.persister;
+
+
+import com.srb.project.model.UsersEntity;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.List;
+
+@Service
+public class LoginServices implements Serializable {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public static final String QUERY_USER = "from UsersEntity where userName=:userName AND password2=:password2";
+
+    public Boolean findUser(UsersEntity user) {
+        Boolean expression = false;
+        List<UsersEntity> entityList = entityManager.createQuery(QUERY_USER, UsersEntity.class)
+                .setParameter("userName", user.getUsername()).setParameter("password2", user.getPassword2()).getResultList();
+        if (!entityList.isEmpty()) {
+            expression = true;
+        }else {
+            expression = false;
+        }
+        return expression;
+    }
+
+    public Boolean findUser(String user, String password) {
+        Boolean expression = false;
+        List<UsersEntity> entityList = entityManager.createQuery(QUERY_USER, UsersEntity.class)
+                .setParameter("userName", user).setParameter("password2", password).getResultList();
+        if (!entityList.isEmpty()) {
+            expression = true;
+        }else {
+            expression = false;
+        }
+        return expression;
+    }
+
+    public boolean validateInfo(String user, String password) {
+        Boolean expression = false;
+        if (user != null && !user.isEmpty() && password != null && !password.isEmpty()) {
+            if (findUser(user, password)) {
+                expression = true;
+            } else {
+                expression = false;
+            }
+        } else {
+            expression = false;
+        }
+        return expression;
+    }
+}
