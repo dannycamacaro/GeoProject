89c8cb9613d2198753c5c57ece6979265bfa8e44
Ericka <monteroh.ericka@gmail.com>
2018-10-15 06:13:13 -0500

    Funcionalidad:CRUD Asignar vehiculo

---
+++ project/src/main/java/com/srb/project/persister/ServicesAssignedVehicle.java
@@ -0,0 +1,36 @@
+package com.srb.project.persister;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+/**
+ * Created by Ericka on 13-10-2018.
+ */
+@Service
+public class ServicesAssignedVehicle {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional
+    public void save(Object object) {
+        entityManager.persist(object);
+    }
+
+    @Transactional
+    public void update(Object object) {
+        entityManager.merge(object);
+    }
+
+    @Transactional
+    public void delete(Object object) {
+        entityManager.remove(object);
+    }
+
+
+}
