49bf6659da0ea0ef0ca3e0e57a4200996d909140
dacamacaro <dacamacaro@consisint.com>
2018-11-02 13:57:18 -0500

    Modificacion de Menu

--- project/src/main/java/com/srb/project/view/ViewMenu.java
+++ project/src/main/java/com/srb/project/view/ViewMenu.java
@@ -11,12 +11,15 @@ import com.vaadin.server.ThemeResource;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.*;
+import com.vaadin.ui.MenuBar;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
+import java.awt.*;
 
 @UIScope
 
-@Widgetset("com.srb.project.widgetset.VaadinmapsWidgetset")
+@Widgetset("com.srb.project.widgetset.DemoWidgetSet")
 @Title("Menu")
 @SpringView(name = ViewMenu.VIEW_NAME, ui = ViewLogin.class)
 public class ViewMenu extends VerticalLayout implements View {
@@ -24,87 +27,54 @@ public class ViewMenu extends VerticalLayout implements View {
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
+        addComponent(buildMenu());
+    }
 
-        MenuBar menuBarRol = new MenuBar();
-        MenuBar menuBarUser = new MenuBar();
-        MenuBar menuBarRoutes = new MenuBar();
-        MenuBar menuBarRoutesDetail = new MenuBar();
-        MenuBar menuBarVehicle = new MenuBar();
-        MenuBar menuBarDevice = new MenuBar();
+    public static MenuBar buildMenu(){
+        MenuBar mainMenu = new MenuBar();
 
-        // A top-level menu item that opens a submenu
-        MenuBar.MenuItem drinks = menuBarRol.addItem("Roles", null, new MenuBar.Command() {
+        mainMenu.addItem("Roles",new MenuBar.Command() {
             @Override
             public void menuSelected(MenuBar.MenuItem selectedItem) {
                 UniverseNavigator.navigate(ViewMaintenanceRol.VIEW_NAME);
             }
         });
 
-        menuBarUser.addItem("Usuario", null, new MenuBar.Command() {
+        mainMenu.addItem("Usuario", null, new MenuBar.Command() {
             @Override
             public void menuSelected(MenuBar.MenuItem selectedItem) {
                 UniverseNavigator.navigate(ViewMaintenanceUser.VIEW_NAME);
             }
         });
-        menuBarVehicle.addItem("Vehiculos", null, new MenuBar.Command() {
+        mainMenu.addItem("Vehiculos", null, new MenuBar.Command() {
             @Override
             public void menuSelected(MenuBar.MenuItem selectedItem) {
                 UniverseNavigator.navigate(ViewMaintenanceVehicle.VIEW_NAME);
             }
         });
 
-        menuBarRoutes.addItem("Rutas", null, new MenuBar.Command() {
+        mainMenu.addItem("Rutas", null, new MenuBar.Command() {
             @Override
             public void menuSelected(MenuBar.MenuItem selectedItem) {
                 UniverseNavigator.navigate(ViewMaintenanceRoutes.VIEW_NAME);
             }
         });
 
-        menuBarRoutesDetail.addItem("Detalle de rutas", null, new MenuBar.Command() {
+        mainMenu.addItem("Detalle de rutas", null, new MenuBar.Command() {
             @Override
             public void menuSelected(MenuBar.MenuItem selectedItem) {
                 UniverseNavigator.navigate(ViewMaintenanceRoutesDetail.VIEW_NAME);
             }
         });
 
-        menuBarDevice.addItem("Dispositivos", null, new MenuBar.Command() {
+        mainMenu.addItem("Dispositivos", null, new MenuBar.Command() {
             @Override
             public void menuSelected(MenuBar.MenuItem selectedItem) {
                 UniverseNavigator.navigate(ViewMaintenanceDevice.VIEW_NAME);
             }
         });
 
-//// Submenu item with a sub-submenu
-//        MenuBar.MenuItem hots = drinks.addItem("Hot", null, null);
-//        hots.addItem("Tea",    mycommand);
-//        hots.addItem("Coffee", mycommand);
-//
-//// Another submenu item with a sub-submenu
-//        MenuBar.MenuItem colds = drinks.addItem("Cold", null, null);
-//        colds.addItem("Milk", null, new MenuBar.Command() {
-//            @Override
-//            public void menuSelected(MenuBar.MenuItem selectedItem) {
-//                UniverseNavigator.navigate(ViewMaintenanceRol.VIEW_NAME);
-//            }
-//        });
-//        colds.addItem("Weissbier", null, mycommand);
-//
-//// Another top-level item
-//        MenuBar.MenuItem snacks = barmenu.addItem("Snacks", null, null);
-//        snacks.addItem("Weisswurst", null, mycommand);
-//        snacks.addItem("Bratwurst",  null, mycommand);
-//        snacks.addItem("Currywurst", null, mycommand);
-//
-//// Yet another top-level item
-//        MenuBar.MenuItem servs = barmenu.addItem("Services", null, null);
-//        servs.addItem("Car Service", null, mycommand);
-
-
-        addComponent(menuBarRol);
-        addComponent(menuBarUser);
-        addComponent(menuBarVehicle);
-        addComponent(menuBarRoutes);
-        addComponent(menuBarRoutesDetail);
-        addComponent(menuBarDevice);
+        return mainMenu;
     }
+
 }
