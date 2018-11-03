f8ee0d6a6a6683a2fa251b0279e1f41cf8e2af55
Ericka <monteroh.ericka@gmail.com>
2018-11-03 12:14:21 -0500

    Funcionalidad: Ajuste en la pantalla de mantenimiento de roles

--- project/src/main/java/com/srb/project/view/ViewMaintenanceRol.java
+++ project/src/main/java/com/srb/project/view/ViewMaintenanceRol.java
@@ -21,30 +21,81 @@ import java.util.Collection;
 @SpringView(name = ViewMaintenanceRol.VIEW_NAME)
 public class ViewMaintenanceRol extends VerticalLayout implements View {
     public static final String VIEW_NAME = "rol";
-    private static final String FIELD_WIDTH = "250px";
+    private HorizontalSplitCrudLayout horizontalSplitCrudLayout;
+    private GridCrud<RolesEntity> crud;
+    private GridLayoutCrudFormFactory<RolesEntity> formFactory;
 
     @Autowired
     ControllerRol controllerRol;
 
     public ViewMaintenanceRol() {
+        horizontalSplitCrudLayout = new HorizontalSplitCrudLayout();
+        horizontalSplitCrudLayout.setFormCaption(CrudOperation.DELETE,EnumLabel.ELIMINAR_REGISTRO_LABEL.getLabel());
+        crud = new GridCrud<>(RolesEntity.class, horizontalSplitCrudLayout);
+        formFactory = new GridLayoutCrudFormFactory<>(RolesEntity.class, 2, 2);
+        buildForm();
 
-        GridCrud<RolesEntity> crud = new GridCrud<>(RolesEntity.class, new HorizontalSplitCrudLayout());
-        GridLayoutCrudFormFactory<RolesEntity> formFactory = new GridLayoutCrudFormFactory<>(RolesEntity.class, 2, 2);
+    }
 
+    public void buildForm() {
+        this.setSizeFull();
+        this.setWidth("100%");
+
+        loadSetColumns();
+        loadSetVisibleProperties();
+        loadSetFieldCaptions();
+        loadSetButtonCaption();
+        loadMessagesForm();
+        actionButtons();
+
+        crud.setCrudFormFactory(formFactory);
+        this.addComponent(crud);
+
+
+    }
+
+    private void loadSetColumns() {
+
+        crud.getGrid().setColumns("namerole", "descriptionrole");
+        crud.getGrid().getColumn("namerole").setCaption("Nombre del rol");
+        crud.getGrid().getColumn("descriptionrole").setCaption("Descripcion");
+    }
+
+    private void loadSetVisibleProperties() {
         formFactory.setVisibleProperties(CrudOperation.READ, "namerole", "descriptionrole");
         formFactory.setVisibleProperties(CrudOperation.ADD, "namerole", "descriptionrole");
         formFactory.setVisibleProperties(CrudOperation.UPDATE, "namerole", "descriptionrole");
         formFactory.setVisibleProperties(CrudOperation.DELETE, "namerole", "descriptionrole");
+    }
+
+    private void loadSetFieldCaptions() {
+        formFactory.setFieldCaptions(CrudOperation.READ, "Nombre del rol", "Descripcion del rol");
         formFactory.setFieldCaptions(CrudOperation.ADD, "Nombre del rol", "Descripcion del rol");
         formFactory.setFieldCaptions(CrudOperation.UPDATE, "Nombre del rol", "Descripcion del rol");
         formFactory.setFieldCaptions(CrudOperation.DELETE, "Nombre del rol", "Descripcion del rol");
+    }
+
+    private void loadSetButtonCaption() {
+        formFactory.setButtonCaption(CrudOperation.READ,EnumLabel.ACEPTAR_LABEL.getLabel());
         formFactory.setButtonCaption(CrudOperation.ADD, EnumLabel.REGISTRAR_LABEL.getLabel());
         formFactory.setButtonCaption(CrudOperation.UPDATE, EnumLabel.EDITAR_LABEL.getLabel());
         formFactory.setButtonCaption(CrudOperation.DELETE, EnumLabel.ELIMINAR_LABEL.getLabel());
-        crud.setRowCountCaption("%d roles(s) encontrados");
-        crud.getGrid().setColumns("namerole", "descriptionrole");
-        crud.getGrid().getColumn("namerole").setCaption("Nombre del rol");
-        crud.getGrid().getColumn("descriptionrole").setCaption("Descripcion");
+        formFactory.setCancelButtonCaption(EnumLabel.CANCELAR_LABEL.getLabel());
+
+    }
+
+    private void loadMessagesForm() {
+
+        crud.setSavedMessage(EnumLabel.REGISTRO_SALVADO_LABEL.getLabel());
+        crud.setDeletedMessage(EnumLabel.REGISTRO_ELIMINADO_LABEL.getLabel());
+        crud.setRowCountCaption(EnumLabel.ROW_COUNT_CAPTION_LABEL.getLabel());
+        crud.getFindAllButton().setDescription(EnumLabel.REFRESCAR_LABEL.getLabel());
+        crud.getAddButton().setDescription(EnumLabel.REGISTRAR_LABEL.getLabel());
+        crud.getUpdateButton().setDescription(EnumLabel.EDITAR_LABEL.getLabel());
+        crud.getDeleteButton().setDescription(EnumLabel.ELIMINAR_LABEL.getLabel());
+    }
+
+    private void actionButtons() {
         crud.setCrudListener(new CrudListener<RolesEntity>() {
             @Override
             public Collection<RolesEntity> findAll() {
@@ -69,17 +120,5 @@ public class ViewMaintenanceRol extends VerticalLayout implements View {
                 controllerRol.deleteRol(user);
             }
         });
-        crud.setCrudFormFactory(formFactory);
-        this.addComponent(crud);
-
-
-    }
-
-    private RolesEntity objectDummy() {
-        RolesEntity rolesEntity = new RolesEntity();
-        rolesEntity.setIdrol(1);
-        rolesEntity.setNamerole("Administrador");
-        rolesEntity.setDescriptionrole("Administrador del sistema");
-        return rolesEntity;
     }
 }
