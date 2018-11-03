0681147926262dfff5d9cd588269e8a7f765b358
Ericka <monteroh.ericka@gmail.com>
2018-11-03 12:03:23 -0500

    Funcionalidad: Ajuste en la pantalla de mantenimiento de vehiculo

--- project/src/main/java/com/srb/project/enumConstans/EnumLabel.java
+++ project/src/main/java/com/srb/project/enumConstans/EnumLabel.java
@@ -4,6 +4,12 @@ public enum EnumLabel {
     REGISTRAR_LABEL("Registrar"),
     EDITAR_LABEL("Editar"),
     ELIMINAR_LABEL("Eliminar"),
+    ACEPTAR_LABEL("Aceptar"),
+    REGISTRO_SALVADO_LABEL("Registro salvado"),
+    REGISTRO_ELIMINADO_LABEL("Registro eliminado"),
+    ROW_COUNT_CAPTION_LABEL("%d registro(s) encontrados"),
+    REFRESCAR_LABEL("Refrescar"),
+    ELIMINAR_REGISTRO_LABEL("¿Desea eliminar el registro?"),
    CANCELAR_LABEL("Cancelar");
 
     EnumLabel(String label) {
--- project/src/main/java/com/srb/project/view/ViewMaintenanceVehicle.java
+++ project/src/main/java/com/srb/project/view/ViewMaintenanceVehicle.java
@@ -21,14 +21,21 @@ import java.util.Collection;
 
 @UIScope
 @SpringView(name = ViewMaintenanceVehicle.VIEW_NAME)
-public class ViewMaintenanceVehicle  extends VerticalLayout implements View {
+public class ViewMaintenanceVehicle extends VerticalLayout implements View {
 
     public static final String VIEW_NAME = "vehicle";
+    private HorizontalSplitCrudLayout horizontalSplitCrudLayout;
+    private GridCrud<VehicleEntity> crud;
+    private GridLayoutCrudFormFactory<VehicleEntity> formFactory;
 
     @Autowired
     ControllerVehicle controllerVehicle;
 
     public ViewMaintenanceVehicle() {
+        horizontalSplitCrudLayout = new HorizontalSplitCrudLayout();
+        horizontalSplitCrudLayout.setFormCaption(CrudOperation.DELETE,EnumLabel.ELIMINAR_REGISTRO_LABEL.getLabel());
+        crud = new GridCrud<>(VehicleEntity.class, horizontalSplitCrudLayout);
+        formFactory = new GridLayoutCrudFormFactory<>(VehicleEntity.class, 2, 2);
         buildForm();
 
     }
@@ -37,29 +44,61 @@ public class ViewMaintenanceVehicle  extends VerticalLayout implements View {
         this.setSizeFull();
         this.setWidth("100%");
 
-        FormLayout formVehicle = new FormLayout();
-        GridCrud<VehicleEntity> crud = new GridCrud<>(VehicleEntity.class, new HorizontalSplitCrudLayout());
-        GridLayoutCrudFormFactory<VehicleEntity> formFactory = new GridLayoutCrudFormFactory<>(VehicleEntity.class, 2, 2);
+        loadSetColumns();
+        loadSetVisibleProperties();
+        loadSetFieldCaptions();
+        loadSetButtonCaption();
+        loadMessagesForm();
+        actionButtons();
 
-        crud.getGrid().setColumns("mark","licenseplate","vehicleyear","ton");
+        crud.setCrudFormFactory(formFactory);
+        this.addComponent(crud);
+
+    }
+
+    private void loadSetColumns() {
+
+        crud.getGrid().setColumns("mark", "licenseplate", "vehicleyear", "ton");
         crud.getGrid().getColumn("mark").setCaption("Marca");
         crud.getGrid().getColumn("licenseplate").setCaption("Placa");
         crud.getGrid().getColumn("vehicleyear").setCaption("Año");
         crud.getGrid().getColumn("ton").setCaption("Carga");
+    }
 
+    private void loadSetVisibleProperties() {
+        formFactory.setVisibleProperties(CrudOperation.READ, "mark", "licenseplate", "vehicleyear", "ton");
+        formFactory.setVisibleProperties(CrudOperation.ADD, "mark", "licenseplate", "vehicleyear", "ton");
+        formFactory.setVisibleProperties(CrudOperation.UPDATE, "mark", "licenseplate", "vehicleyear", "ton");
+        formFactory.setVisibleProperties(CrudOperation.DELETE, "mark", "licenseplate", "vehicleyear", "ton");
+    }
 
-        formFactory.setVisibleProperties(CrudOperation.READ, "mark","licenseplate","vehicleyear","ton");
-        formFactory.setVisibleProperties(CrudOperation.ADD, "mark","licenseplate","vehicleyear","ton");
-        formFactory.setVisibleProperties(CrudOperation.UPDATE, "mark","licenseplate","vehicleyear","ton");
-        formFactory.setVisibleProperties(CrudOperation.DELETE, "mark","licenseplate","vehicleyear","ton");
-        formFactory.setFieldCaptions(CrudOperation.ADD, "Marca del vehiculo","Placa", "Año del vehiculo","Carga");
-        formFactory.setFieldCaptions(CrudOperation.UPDATE, "Marca del vehiculo","Placa", "Año del vehiculo","Carga");
-        formFactory.setFieldCaptions(CrudOperation.DELETE, "Marca del vehiculo","Placa", "Año del vehiculo","Carga");
+    private void loadSetFieldCaptions() {
+        formFactory.setFieldCaptions(CrudOperation.READ, "Marca del vehiculo", "Placa", "Año del vehiculo", "Carga");
+        formFactory.setFieldCaptions(CrudOperation.ADD, "Marca del vehiculo", "Placa", "Año del vehiculo", "Carga");
+        formFactory.setFieldCaptions(CrudOperation.UPDATE, "Marca del vehiculo", "Placa", "Año del vehiculo", "Carga");
+        formFactory.setFieldCaptions(CrudOperation.DELETE, "Marca del vehiculo", "Placa", "Año del vehiculo", "Carga");
+    }
+
+    private void loadSetButtonCaption() {
+        formFactory.setButtonCaption(CrudOperation.READ,EnumLabel.ACEPTAR_LABEL.getLabel());
         formFactory.setButtonCaption(CrudOperation.ADD, EnumLabel.REGISTRAR_LABEL.getLabel());
         formFactory.setButtonCaption(CrudOperation.UPDATE, EnumLabel.EDITAR_LABEL.getLabel());
         formFactory.setButtonCaption(CrudOperation.DELETE, EnumLabel.ELIMINAR_LABEL.getLabel());
-        formFactory.setCancelButtonCaption("Cancelar");
+        formFactory.setCancelButtonCaption(EnumLabel.CANCELAR_LABEL.getLabel());
+    }
 
+    private void loadMessagesForm() {
+
+        crud.setSavedMessage(EnumLabel.REGISTRO_SALVADO_LABEL.getLabel());
+        crud.setDeletedMessage(EnumLabel.REGISTRO_ELIMINADO_LABEL.getLabel());
+        crud.setRowCountCaption(EnumLabel.ROW_COUNT_CAPTION_LABEL.getLabel());
+        crud.getFindAllButton().setDescription(EnumLabel.REFRESCAR_LABEL.getLabel());
+        crud.getAddButton().setDescription(EnumLabel.REGISTRAR_LABEL.getLabel());
+        crud.getUpdateButton().setDescription(EnumLabel.EDITAR_LABEL.getLabel());
+        crud.getDeleteButton().setDescription(EnumLabel.ELIMINAR_LABEL.getLabel());
+    }
+
+    private void actionButtons() {
         crud.setCrudListener(new CrudListener<VehicleEntity>() {
             @Override
             public Collection<VehicleEntity> findAll() {
@@ -84,12 +123,5 @@ public class ViewMaintenanceVehicle  extends VerticalLayout implements View {
                 controllerVehicle.deleteVehicle(vehicle);
             }
         });
-
-        crud.setCrudFormFactory(formFactory);
-        this.addComponent(crud);
-
-
     }
-
-
 }
